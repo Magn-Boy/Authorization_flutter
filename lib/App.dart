02a2fff005ccb45092fb5d81124542d0da07e6fe
Magn-Boy <andruha.vasilev.2004@mail.ru>
2023-11-09 13:48:52 +0300

    feat: Add valifdator and navigator in button

--- lib/App.dart
+++ lib/App.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Primer.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -25,6 +26,8 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
+    bool _isPasswordVisible = false;
+   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -36,97 +39,128 @@ class _AppState extends State<App> {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(top: 70),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 150,
-                  height: 150,
-                  child: Image.asset('assets/images/adidas.png'),
-                ),
-                SizedBox(height: 35),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: Container(
-                    child: TextField(
-                      controller: _emailController,
-                      focusNode: _emailFocusNode,
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.circular(35),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 70),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    child: Image.asset('assets/images/adidas.png'),
+                  ),
+                  SizedBox(height: 35),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Container(
+                      child: TextFormField(
+                        validator: (value) {
+                          if(value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+
+                          bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+                          if(!emailValid){
+                            return 'Please enter valid email';
+                          }
+                          return null;
+                        },
+                        controller: _emailController,
+                        focusNode: _emailFocusNode,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(35),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(35),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: 'Email',
+                          hintText: 'Enter your email',
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black.withOpacity(0.7),
+                          ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(35),
-                          borderSide: BorderSide(color: Colors.black),
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelText: 'Email',
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black.withOpacity(0.7),
-                        ),
+                        obscureText: true,
                       ),
-                      obscureText: true,
                     ),
                   ),
-                ),
-                SizedBox(height: 35),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: Container(
-                    child: TextField(
-                      controller: _passwordController,
-                      focusNode: _passwordFocusNode,
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.circular(35),
+                  SizedBox(height: 35),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Container(
+                      child: TextFormField(
+                        validator: (value){
+                          if(value == null || value.isEmpty){
+                            return 'Please enter some text';
+                          }
+                          if(value.length < 6){
+                            return 'Password must be at least 6 characters';
+                          }
+                          return null;
+                        },
+                        controller: _passwordController,
+                        focusNode: _passwordFocusNode,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(35),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(35),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: 'Password',
+                          hintText: 'Enter your password',
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black.withOpacity(0.7),
+                          ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(35),
-                          borderSide: BorderSide(color: Colors.black),
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelText: 'Password',
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black.withOpacity(0.7),
-                        ),
+                        obscureText: !_isPasswordVisible,
+                        
                       ),
-                      obscureText: true,
                     ),
                   ),
-                ),
-                SizedBox(height: 50),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: Container(
-                    width: double.infinity,
-                    height: 60,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print('Кнопка нажата');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(35),
+                  SizedBox(height: 50),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Container(
+                      width: double.infinity,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if(_formKey.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => NewScreen())
+                              );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(35),
+                          ),
+                          elevation: 20,
+                          primary: Colors.black,
+                          onPrimary: Colors.white,
                         ),
-                        elevation: 20,
-                        primary: Colors.black,
-                        onPrimary: Colors.white,
+                        child: Text('Войти'),
                       ),
-                      child: Text('Войти'),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/Primer.dart
+++ lib/Primer.dart
@@ -1,75 +1,22 @@
-// import 'package:flutter/material.dart';
-//
-// class Primer extends StatefulWidget {
-//   const App({Key? key}) : super(key: key);
-//
-//   @override
-//   State<App> createState() => _AppState();
-// }
-//
-// class _AppState extends State<App> {
-//   TextEditingController _controller = TextEditingController();
-//   FocusNode _focusNode = FocusNode();
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller.addListener(onTextChanged);
-//   }
-//
-//   void onTextChanged() {
-//     print("Текст изменен: ${_controller.text}");
-//   }
-//
-//   void onSubmit(String text) {
-//     print("Текст отправлен: $text");
-//   }
-//
-//   void setFocus() {
-//     setState(() {
-//       FocusScope.of(context).requestFocus(_focusNode);
-//     });
-//   }
-//
-//   void clearText() {
-//     setState(() {
-//       _controller.clear();
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Пример TextField')),
-//       body: Padding(
-//         padding: EdgeInsets.all(16),
-//         child: Column(
-//           children: [
-//             TextField(
-//               controller: _controller,
-//               focusNode: _focusNode,
-//               decoration: InputDecoration(
-//                 labelText: 'Email',
-//                 border: OutlineInputBorder(),
-//               ),
-//               onChanged: (text) {
-//                 print("Изменился текст: $text");
-//               },
-//               onSubmitted: onSubmit,
-//             ),
-//             SizedBox(height: 16),
-//             ElevatedButton(
-//               onPressed: setFocus,
-//               child: Text('Установить фокус'),
-//             ),
-//             SizedBox(height: 16),
-//             ElevatedButton(
-//               onPressed: clearText,
-//               child: Text('Очистить текст'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class NewScreen extends StatefulWidget {
+  const NewScreen({Key? key}) : super(key: key); // Added Key? key parameter
+
+  @override
+  State<NewScreen> createState() => _NewScreenState();
+}
+
+class _NewScreenState extends State<NewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Screen"), // Changed title to Text widget
+      ),
+      body: Center(
+        child: Text("Welcome"),
+      ),
+    );
+  }
+}
